e219d3a03f51382b1e78c9b26a993b392eff2a60
L.D <61159151+Roarkiner@users.noreply.github.com>
2023-04-14 01:38:38 +0200

    Notification when app in background + dynamic layout change

---
+++ app/src/main/java/com/example/challenge48h/MachineStatus.kt
@@ -0,0 +1,3 @@
+package com.example.challenge48h
+
+data class MachineStatus(var coffee: Int)
--- app/src/main/java/com/example/challenge48h/MainActivity.kt
+++ app/src/main/java/com/example/challenge48h/MainActivity.kt
@@ -1,32 +1,70 @@
 package com.example.challenge48h
 
-import android.content.Context
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import androidx.core.content.ContextCompat;
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.animation.AnimationUtils
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import com.example.challenge48h.manager.CoffeeApiManager
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val machineStatusCheckManager: MachineStatusCheckManager = MachineStatusCheckManager(this)
+    private var coffeeMachineStatus: Int = 0
+    private lateinit var handler: Handler
+    private lateinit var runnable: Runnable
+    private var isActivityActive = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val coffeeMachineManager = CoffeeApiManager()
-        val coffeeMachineStatus = coffeeMachineManager.GetCoffeeMachineStatus()
+        coffeeMachineStatus = machineStatusCheckManager.checkMachineStatus()
+        machineStatusCheckManager.createDataFileIfDoesntExist(coffeeMachineStatus)
 
+        setLayoutStyleForCurrentStatus()
+        createNotificationChannel()
+
+        handler = Handler(Looper.getMainLooper())
+        runnable = Runnable {
+            Log.d("Loop Occurence before", coffeeMachineStatus.toString())
+            checkIfStatusHasChanged()
+            Log.d("Loop Occurence after", coffeeMachineStatus.toString())
+            handler.postDelayed(runnable, 2000)
+        }
+
+        handler.post(runnable)
+    }
+
+    private fun checkIfStatusHasChanged() {
+        val newStatus: Int? = machineStatusCheckManager.checkIfStatusChanged()
+        if(newStatus != null){
+            coffeeMachineStatus = newStatus
+            if(isActivityActive){
+                setLayoutStyleForCurrentStatus()
+            } else {
+                showNotification()
+            }
+        }
+    }
+
+    private fun setLayoutStyleForCurrentStatus(){
         val mainContainer = findViewById<LinearLayout>(R.id.main_page_container)
         val mainIcon = findViewById<ImageView>(R.id.main_page_icon)
         val mainStatusText = findViewById<TextView>(R.id.main_page_status_text)
 
         startRotationAnimation(mainIcon)
 
-        if(coffeeMachineStatus){
+        if(coffeeMachineStatus == 1){
             mainContainer.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
             mainIcon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_coffee))
             mainStatusText.text = "La machine a café fonctionne"
@@ -37,8 +75,52 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showNotification(){
+        var notifIcon = R.drawable.ic_coffee_invalid
+        var notifDesc = "La machine a café est en panne"
+        if(coffeeMachineStatus == 1){
+            notifIcon = R.drawable.ic_coffee
+            notifDesc = "La machine a café est de nouveau opérationnelle"
+        }
+
+        val date = Date()
+        val notificationId = SimpleDateFormat("ddhhmmss", Locale.FRANCE).format(date).toInt()
+        val notificationBuilder = NotificationCompat.Builder(this, "channel01")
+            .setSmallIcon(notifIcon)
+            .setContentTitle("Le statut de la machine à café a changé")
+            .setContentText(notifDesc)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        val notificationManagerCompat = NotificationManagerCompat.from(this)
+        notificationManagerCompat.notify(notificationId, notificationBuilder.build())
+    }
+
+    private fun createNotificationChannel(){
+        val name: CharSequence = "CoffeeMachine"
+        val description = "Coffee machine notifications channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val notificationChannel = NotificationChannel("channel01", name, importance)
+        notificationChannel.description = description
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+
     private fun startRotationAnimation(imageView: ImageView) {
         val rotationAnimation = AnimationUtils.loadAnimation(this, R.anim.spin_center_animation)
         imageView.animation = rotationAnimation
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isActivityActive = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isActivityActive = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(runnable)
     }
 }
